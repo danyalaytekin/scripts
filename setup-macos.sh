44725f23c6578b18ed537fa897ab5826637c7231
Danyal Aytekin <danyal@fubase.com>
2017-11-09 15:32:31 +0000

    Add macos setup script

---
+++ setup-macos.sh
@@ -0,0 +1,69 @@
+# Homebrew - setup
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update && brew doctor
+
+# Homebrew - run installers
+brew install \
+    autojump \
+    chruby \
+    curl \
+    ethereum \
+    ghi \
+    mono \
+    mplayer \
+    nvm \
+    ruby-install \
+    tree \
+    wget
+
+# Homebrew casks - run installers
+brew cask install \
+    balsamiq-mockups \
+    beyond-compare \
+    bitbar \
+    charles \
+    coconutbattery \
+    daisydisk \
+    dropbox \
+    firefox \
+    gitter \
+    google-chrome \
+    itsycal \
+    mas \
+    mist \
+    postman \
+    sketch \
+    sourcetree \
+    spotify \
+    steam \
+    textexpander \
+    tidal \
+    transmission \
+    visual-studio-code \
+    vmware-fusion
+
+# Chrome - disable navigation gestures
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
+
+# Mac App Store - run installers
+mas install \
+    620249105 `#LaunchD` \
+    409183694 `#Keynote` \
+    441258766 `#Magnet` \
+    634210019 `#Plisterine` \
+    434808346 `#SimpleMind` \
+    682658836 `#GarageBand` \
+    407963104 `#Pixelmator` \
+    443987910 `#1Password` \
+    880001334 `#Reeder` \
+    497799835 `#Xcode` \
+    409203825 `#Numbers` \
+    409201541 `#Pages` \
+    1147396723 `#WhatsApp` \
+    1091189122 `#Bear` \
+    408981434 `#iMovie` \
+    587769990 `#EyeFriendly` \
+    1176895641 `#Spark` \
+    803453959 `#Slack` \
+    1225570693 `#UlyssesMac` \
+    1026566364 `#GoodNotes`
