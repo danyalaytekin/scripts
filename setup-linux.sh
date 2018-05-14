eb7073b1897064be8ba3bcffe9606fdd0d334f1a
Danyal Aytekin <danyal@fubase.com>
2018-05-14 03:53:59 +0100

    On Linux install apt packages

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,2 +1,10 @@
 # Create working directories
 mkdir ~/Projects
+
+# apt - install packages
+sudo apt install \
+    autojump \
+    libimage-exiftool-perl \
+    lynx \
+    redis-server \
+    translate-shell
